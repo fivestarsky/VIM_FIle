acb6dfc1121df0d4365a8f8574b844e963a7b0b1
五星六日 <fivestarsky@gmail.com>
2015-07-29 01:10:15 +0800

    Create setup_zsh_01.sh

---
+++ setup_zsh_01.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd ~/
+
+yum install zsh autojump autojump-zsh wget tar ctags ctags-etags git python-devel cmake g++ gcc-c++ -y
+sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
