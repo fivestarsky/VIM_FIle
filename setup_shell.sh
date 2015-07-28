4e1ba434f165370c35c3a1523acea33d69161de7
五星六日 <fivestarsky@gmail.com>
2015-07-29 01:09:33 +0800

    Update setup_shell.sh

--- setup_shell.sh
+++ setup_shell.sh
@@ -1,11 +1,4 @@
-安装Golang,脚本
----------------------------------------------------------------------------------
 #!/bin/bash
-
-cd ~/
-
-yum install zsh autojump autojump-zsh wget tar ctags ctags-etags git python-devel cmake g++ gcc-c++ -y
-sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
 wget https://raw.githubusercontent.com/fivestarsky/VIM_FIle/master/vimrc 
 mv ~/vimrc ~/.vimrc
@@ -30,13 +23,3 @@ source ~/.bashrc
 mkdir ~/.vim/colors
 cd ~/.vim/colors
 wget https://raw.githubusercontent.com/fatih/molokai/master/colors/molokai.vim
-
-#重启系统
-#---------------------------------------------------------------------------------
-#vim ~/.vimrc
-#注释掉~/.vimrc最后两行
-#退出 vim
-
-#cd ~/.vim/bundle/YouCompleteMe/
-#./install.sh --gocode-completer
-#---------------------------------------------------------------------------------
