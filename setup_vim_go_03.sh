3de3f86cba4c3d030985de3d90d5f3f29029147e
五星六日 <fivestarsky@gmail.com>
2015-07-29 01:26:57 +0800

    Update setup_vim_go_03.sh

--- setup_vim_go_03.sh
+++ setup_vim_go_03.sh
@@ -1,9 +1,23 @@
 #重启系统
 #---------------------------------------------------------------------------------
 #vim ~/.vimrc
-#执行~/.vimrc最后两行，冒号空间命令
+#在vim冒号空间执行 ~/.vimrc 倒数第二行命令
 #退出 vim
 
 #cd ~/.vim/bundle/YouCompleteMe/
 #./install.sh --gocode-completer
+
+#vim ~/.vimrc
+#在vim冒号空间执行 ~/.vimrc 倒数第一行命令
+#退出 vim
+
+#go get github.com/Unknwon/macaron
+
+进入vim 
+F2        目录 
+F8        Tags
+F3 + 上下左右 切换window
+Ctrl + a or Ctrl + d 水平调整window宽度
+Ctrl + z or Ctrl + c 水平调整window高度
+
 #---------------------------------------------------------------------------------
